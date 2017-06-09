1dcc166b2dac373f4d027e349c406bafa5c3a17e
Joerg Schmitz <schmitz873@gmail.com>
2017-06-10 00:25:22 +0200

    Basic Core Module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
@@ -13,13 +12,11 @@ import { AuthGuard } from './auth/auth-guard.service';
 import { SharedModule } from "app/shared/shared.module";
 import { ShoppingListModule } from "app/shopping-list/shopping-list.module";
 import { AuthModule } from "app/auth/auth.module";
-import { HomeComponent } from './home/home.component';
+import { CoreModule } from "app/core/core.module";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -27,6 +24,7 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     ShoppingListModule,
     AuthModule,
+    CoreModule,
     SharedModule
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
