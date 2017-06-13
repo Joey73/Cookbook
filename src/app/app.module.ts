8bbcd8653c487b8bcc8b3990f9ace1330dfe2383
Joerg Schmitz <schmitz873@gmail.com>
2017-06-14 00:07:04 +0200

    Added BrowserAnimationsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -20,7 +21,8 @@ import { CoreModule } from "app/core/core.module";
     ShoppingListModule,
     AuthModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    BrowserAnimationsModule
   ],
   bootstrap: [AppComponent]
 })
